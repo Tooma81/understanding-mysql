c4488bbafbf1ed8b77082a653910e93928dac6e1
tooma81 <toomas.kaarel@gmail.com>
2023-12-13 11:03:51 +0200

    added ALTER TABLE

--- demo_create_table.js
+++ demo_create_table.js
@@ -16,4 +16,9 @@ con.connect(function(err) {
         if (err) throw err;
         console.log("Table created");
     })
+    var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table altered");
+    })
 });
